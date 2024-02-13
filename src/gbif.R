db9eb728cf7d67afcd6ab24ea59718dab0989b4f
Jeremy McWilliams <jeremym@lclark.edu>
2024-02-13 20:38:26 +0000

    Downloaded gbif data!

--- src/gbif.R
+++ src/gbif.R
@@ -3,3 +3,55 @@
 # clean up the data
 # save it to a csv file
 # create a map to display the species occurrence points
+
+#list of packages
+packages<-c("tidyverse", "rgbif", "usethis", "CoordinateCleaner", "leaflet", "mapview")
+
+# install packages not yet installed
+installed_packages<-packages %in% rownames(installed.packages())
+if(any(installed_packages==FALSE)){
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading, with library function
+invisible(lapply(packages, library, character.only=TRUE))
+
+
+usethis::edit_r_environ()
+
+spiderBackbone<-name_backbone(name="Habronattus americanus")
+speciesKey<-spiderBackbone$usageKey
+
+occ_download(pred("taxonKey", speciesKey), format="SIMPLE_CSV")
+
+<<gbif download>>
+#Your download is being processed by GBIF:
+#  https://www.gbif.org/occurrence/download/0011249-240202131308920
+#Most downloads finish within 15 min.
+#Check status with
+#occ_download_wait('0011249-240202131308920')
+#After it finishes, use
+
+#to retrieve your download.
+#Download Info:
+#  Username: jeremy2443
+#E-mail: jeremym@lclark.edu
+#Format: SIMPLE_CSV
+#Download key: 0011249-240202131308920
+#Created: 2024-02-12T22:52:08.768+00:00
+#Citation Info:  
+#  Please always cite the download DOI when using this data.
+#https://www.gbif.org/citation-guidelines
+#DOI: 10.15468/dl.9wtpm9
+#Citation:
+#  GBIF Occurrence Download https://doi.org/10.15468/dl.9wtpm9 Accessed from R via rgbif (https://github.com/ropensci/rgbif) on 2024-02-12
+
+d <- occ_download_get('0011249-240202131308920', path="data/") %>%
+  occ_download_import()
+
+write_csv(d, "data/rawData.csv")
+
+
+
+
+
